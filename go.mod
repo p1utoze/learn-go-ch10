20471341e6d11d3cdc2063f803a8bfbff9c67dc0
Adithya Awati <aditya.n.awati@gmail.com>
2026-08-15 13:41:40 +0000

    feat: init new public Add function in main

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/p1utoze/learn-go-ch10
+
+go 1.26.1
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+// # This is a simple public function for Adding two numbers.
+//
+// Check out this [link] for in-depth reference.
+//
+// [link]: https://www.mathsisfun.com/numbers/addition.html
+func Add(a, b int) int {
+	return a + b
+}
+
+
+
